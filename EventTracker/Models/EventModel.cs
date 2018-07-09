63061d5a0a6a6a061308f121a11a424f033237bb
Cezary Stasiak <cezary.p.stasiak@gmail.com>
2018-07-09 22:04:07 +0200

    Clean Initial Commit + EventModel

---
+++ EventTracker/Models/EventModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventTracker.Models
+{
+    public class EventModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
