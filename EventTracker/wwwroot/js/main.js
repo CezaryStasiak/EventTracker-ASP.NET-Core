5fb04ebd0e710bc0fbe13b706d5d7d996c155fe4
Cezary Stasiak <cezary.p.stasiak@gmail.com>
2018-07-14 10:50:14 +0200

    Revert "Revert "fixed month selection (front end)""
    This reverts commit 64318a2d37fc4bd8e6af5da8762253ef7097b091.

--- EventTracker/wwwroot/js/main.js
+++ EventTracker/wwwroot/js/main.js
@@ -2,47 +2,70 @@
 window.onload = function () {
 
     //setting year
-    var x = document.getElementById("year");
-    x.innerText = new Date().getFullYear();
-
+    getYear();
     //setting months
-    var monthName = ["January", "February", "March", "April", "May", "June",
-        "July", "August", "September", "October", "November", "December"];
-    var months = document.getElementById("months");
-    for (var i = 0; i < 12; i++) {
-        var x = document.createElement("div");
-        x.id = "month" + i + "button";
-        x.innerText = monthName[i];
-        x.classList += "btn btn-warning month";
-        months.appendChild(x);
-    }
+    setDays();
+    setMonths();
     //setting days for each month
+    
+}
+
+function getYear() {
+    let x = document.getElementById("year");
+    x.innerText = new Date().getFullYear();
+}
+function setDays() {
     var days = document.getElementById("days");
     for (var v = 1; v <= 12; v++) {
-        var m = document.createElement("div");
+        let m = document.createElement("div");
         m.id = "month" + v + "days";
         days.appendChild(m);
         for (var i = 1; i <= daysInMonth(new Date().getFullYear(), v); i++) {
-            var x = document.createElement("div");
+            let x = document.createElement("div");
             x.id = "month" + v + "day" + i + "button";
             x.innerText = i;
             x.classList += "btn btn-info day";
             m.appendChild(x);
         }
     }
+}
 
-    hideDays(new Date().getMonth());
+function setMonths() {
+    var monthName = ["January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"];
+    var months = document.getElementById("months");
+    for (let i = 1; i <= 12; i++) {
+        let x = document.createElement("div");
+        x.id = "month" + i + "button";
+        x.innerText = monthName[i-1];
+        x.classList += "btn btn-warning month";
+        x.addEventListener("click", function () { hideDays(i) } );
+        months.appendChild(x);
+    }
+
+    for (var i = 1; i <= 12; i++) {
+        let d = document.getElementById("month" + i + "days");
+        d.style.display = "none";
+    }
 }
 
 function daysInMonth(year, month) {
     return new Date(year, month, 0).getDate();
 }
 
-function hideDays(id) {
-    for (var i = 1; i <= 12; i++) {
-        var x = document.getElementById("month" + i + "days");
-        if (i != id) {
-        x.style.display = "none";
-        }
-    }
+var func = function () {
+    alert("siema");
 }
+
+var hideDays = function(id) {
+        for (var i = 1; i <= 12; i++) {
+                let d = document.getElementById("month" + i + "days");
+                d.style.display = "none";
+        }
+        let q = document.getElementById("month" + id + "days");
+        q.style.display = "block";
+}
+
+
+
+
